bbdc70591ff8f8581d03bfe9a207cd6504859701
pingingin <86203918+pingingin@users.noreply.github.com>
2022-04-02 19:48:20 +0700

    Update keep_alive.py

--- src/keep_alive.py
+++ src/keep_alive.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from threading import Thread
+#from threading import Thread
 
 app = Flask('')
 
@@ -11,5 +11,6 @@ def run():
   app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
-  t = Thread(target = run)
-  t.start()
+  app.run(host='0.0.0.0', port=8080)
+  #t = Thread(target = run)
+  #t.start()
